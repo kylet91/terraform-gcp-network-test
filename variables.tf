bf51a87846934bc669d63800198e7a875346bd14
kylet91 <34290279+kylet91@users.noreply.github.com>
2020-08-06 22:36:28 +1000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "regions" {
+  default  = {
+    "asia-east1" = 1
+    "asia-east2" = 1
+    "asia-northeast1" = 1
+    "asia-northeast2" = 1
+    "asia-northeast3" = 1
+    "asia-south1" = 1
+    "asia-southeast1" = 1
+    "asia-southeast2" = 1
+    "australia-southeast1" = 1
+    "europe-north1" = 1
+    "europe-west1" = 1
+    "europe-west2" = 1
+    "europe-west3" = 1
+    "europe-west4" = 1
+    "europe-west6" = 1
+    "northamerica-northeast1" = 1
+    "southamerica-east1" = 1
+    "us-central1" = 1
+    "us-east1" = 1
+    "us-east4" = 1
+    "us-west1" = 1
+    "us-west2" = 1
+    "us-west3" = 1
+    "us-west4" = 1
+  }
+}
